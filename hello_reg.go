648925b9e24275b7c5880cc8b563dbfa3bb6b80e
ponxu <xwz689@gmail.com>
2016-02-16 15:31:18 +0800

    test

--- hello_reg.go
+++ hello_reg.go
@@ -10,4 +10,9 @@ func main() {
     r := regexp.MustCompile(`\w+`)
     rs := r.FindAllString(s, -1)
     fmt.Println(rs)
+
+    s = "/abc/123/ggg/123aa"
+    r = regexp.MustCompile("/([a-z]+)/(w+)/([a-z]+)/(.+)")
+    rs2 := r.FindAllStringSubmatch(s, -1)
+    fmt.Println(rs2)
 }
