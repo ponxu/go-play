e0f317845209e59c104387bf55c5a68c20a617db
xuwenzhao <xuwenzhao@apusapps.com>
2016-11-14 10:39:34 +0800

    test

--- hello_gin.go
+++ hello_gin.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "net/http"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    router := gin.Default()
-    router.GET("/", func(c *gin.Context) {
-        c.String(http.StatusOK, "hello world")
-    })
-    router.GET("/ping", func(c *gin.Context) {
-        c.String(http.StatusOK, "pong")
-    })
-    router.POST("/submit", func(c *gin.Context) {
-        c.String(http.StatusUnauthorized, "not authorized")
-    })
-    router.PUT("/error", func(c *gin.Context) {
-        c.String(http.StatusInternalServerError, "and error happened :(")
-    })
-    router.Run(":8080")
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello world")
+	})
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+	router.POST("/submit", func(c *gin.Context) {
+		c.String(http.StatusUnauthorized, "not authorized")
+	})
+	router.PUT("/error", func(c *gin.Context) {
+		c.String(http.StatusInternalServerError, "and error happened :(")
+	})
+	router.Run(":8080")
 }
